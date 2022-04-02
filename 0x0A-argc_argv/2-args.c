ebdf5deee5e343d7107f4bf693e0cd987b0507a7
Nurudeen Hassan <nurudeenbika@gmail.com>
2022-04-02 19:45:22 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the name of the program followed by a
@@ -11,9 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
-		printf("Error\n");
-	else
-		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
